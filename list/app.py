236c2f541a6afdea471b0e53f33e79ef20a3d979
DamarisCaballero <damariscallero011@gmail.com>
2023-05-15 14:33:53 -0600

    changes

--- list/app.py
+++ list/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+titulares = [
+    # Lista de titulares de noticias
+    "Aumenta la demanda de energías renovables",
+    "Desarrollan vacuna efectiva contra el virus del dengue",
+    # ... (agrega los demás titulares de noticias)
+]
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        palabra_clave = request.form['palabra_clave']
+        return redirect(url_for('resultados', palabra_clave=palabra_clave))
+    return render_template('home.html')
+
+@app.route('/resultados')
+def resultados():
+    palabra_clave = request.args.get('palabra_clave')
+    titulares_filtrados = [titular for titular in titulares if palabra_clave.lower() in titular.lower()]
+    return render_template('resultados.html', titulares=titulares_filtrados, palabra_clave=palabra_clave)
 
 if __name__ == '__main__':
     app.run()
